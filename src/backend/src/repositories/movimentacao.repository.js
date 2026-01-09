bc8a76a9d69304953d6bcdb2b6f459acfcdfeb97
Trick-Breno <141360060+Trick-Breno@users.noreply.github.com>
2026-01-09 15:31:39 -0300

    feat: implementa atualização de movimentações e ajuste de saldo de carteiras

--- src/backend/src/repositories/movimentacao.repository.js
+++ src/backend/src/repositories/movimentacao.repository.js
@@ -62,3 +62,69 @@ export const findById = async (id, userId) => {
     const {rows} = await pool.query(query, [id, userId]);
     return rows[0] || null;
 };
+
+export const update = async (id, userId, movimentacaoData ) => {
+    const client = await pool.connect();
+
+    try{
+        await client.query('BEGIN');
+
+        const fields = Object.keys(movimentacaoData);
+        const values = Object.values(movimentacaoData);
+
+        if (fields.length === 0) {
+            await client.query('ROLLBACK');
+            return null;
+        }
+
+        const setClause = fields.map((field, index) => 
+            `${field} = $${index + 1}`).join(', ');
+
+        const queryParams = [...values, id, userId];
+        const idParamIndex = values.length + 1;
+        const userIdParamIndex = values.length + 2;
+
+        const query = `
+        UPDATE movimentacoes SET ${setClause} WHERE id = $${idParamIndex} AND user_id = $${userIdParamIndex}
+        RETURNING*;
+        `;
+
+        const {rows} = await client.query(query, queryParams);
+        const updatedMovimentacao = rows[0];
+
+        if (!updatedMovimentacao) {
+            await client.query('ROLLBACK');
+            return null;
+        }
+
+        const idCarteira = updatedMovimentacao.id_carteira;
+
+        const querySaldo = `
+            UPDATE carteiras 
+            SET saldo_atual = saldo_inicial + (
+                SELECT COALESCE(SUM(
+                    CASE
+                        WHEN tipo = 'receita' THEN valor
+                        WHEN tipo = 'despesa' THEN - valor
+                        ELSE 0
+                    END
+                ), 0)
+                FROM movimentacoes
+                WHERE id_carteira = $1
+                AND status = 'concluido'
+            )
+            WHERE id = $1;
+        `;
+
+        await client.query(querySaldo, [idCarteira]);
+
+        await client.query('COMMIT');
+        return updatedMovimentacao;
+
+    } catch (error) {
+        await client.query('ROLLBACK');
+        throw error;
+    } finally {
+        client.release();
+    }
+};
