41b1f4d8122a69c6cea339e5be15051d30f0636c
Trick-Breno <141360060+Trick-Breno@users.noreply.github.com>
2026-06-03 12:16:32 -0300

    feat: atualiza rotas para usar a página Movimentacoes

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,24 +3,23 @@ import AppLayout from './layouts/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import PrivateRoute from './routes/PrivateRoute';
-import Despesas from './pages/Despesas';
+import Carteiras from './pages/Receitas';
+import Movimentacoes from './pages/Movimentacoes'; // Importando a nova página principal
 
 export default function App() {
   return (
-    <>
     <Routes>
-      
-      {/* Rotas Públicas */}
       <Route path="/login" element={<Login />} />
 
-      {/* Rotas Privadas (Protegidas pelo PrivateRoute) */}
       <Route element={<PrivateRoute />}>
         <Route element={<AppLayout />}>
-          <Route path="/" element={<Despesas />} />
+          
+          <Route path="/" element={<Movimentacoes />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/carteiras" element={<Carteiras />} />
+
         </Route>
       </Route>
     </Routes>
-    </>
   );
 }
