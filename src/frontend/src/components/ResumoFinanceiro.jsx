fb758a247f6816f835d4cd1b0ce810d93e7efa0b
Trick-Breno <141360060+Trick-Breno@users.noreply.github.com>
2026-07-01 18:05:14 -0300

    fix: add future balance calculation to ResumoFinanceiro component

--- src/frontend/src/components/ResumoFinanceiro.jsx
+++ src/frontend/src/components/ResumoFinanceiro.jsx
@@ -30,6 +30,11 @@ export default function ResumoFinanceiro()  {
                         R$ {Number(resumoDespesas?.falta || 0).toFixed(2)}
                     </span>
                 </li>
+                <li className="flex gap-6 justify-between">
+                    <span className=" text-gray-700">Saldo Futuro</span>
+                    <span className=" text-gray-700">R$ {Number(saldoCarteiras.total - resumoDespesas.total).toFixed(2)}</span>
+                </li>
+
             </ul>
 
         </div>
